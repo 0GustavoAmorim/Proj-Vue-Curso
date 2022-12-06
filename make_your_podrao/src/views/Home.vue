893bb23df9bfcf6d8c249ca4232561586dcd02b8
Gustavo Amorim <89614046+0GustavoAmorim@users.noreply.github.com>
2022-12-06 10:35:18 -0300

    Add files via upload

---
+++ make_your_podrao/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+      <Banner />
+  <div class="main-container">
+    <h1>
+      Monte seu Podrão
+    </h1>
+
+    <BurguerForm />
+  </div>
+</template>
+
+<script>
+  import Banner from "../components/Banner.vue"
+  import BurguerForm from "../components/BurguerForm.vue";
+
+  export default{
+    name: "Home",
+    components: {
+      Banner,
+      BurguerForm
+    }
+  }
+</script>
